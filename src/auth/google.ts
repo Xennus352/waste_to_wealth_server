72c004add143dd0f2492ca8f98ea8107eb7b8564
Xennus352 <xennus.dev@gmail.com>
2025-04-16 23:58:12 +0630

    production1

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -8,7 +8,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "http://localhost:8000/auth/google/callback",
+      // callbackURL: "http://localhost:8000/auth/google/callback",
+      callbackURL:
+        "https://waste-to-wealth-server.vercel.app/auth/google/callback",
       passReqToCallback: true,
     },
     async function (request, accessToken, refreshToken, profile, done) {
